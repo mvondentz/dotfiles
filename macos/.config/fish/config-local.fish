b6200b780431114699a15dcc17ba6bacce2602d2
Mauricio Von Dentz <mauriciovondentz@gmail.com>
2022-05-24 20:44:22 -0300

    macos stuff

---
+++ macos/.config/fish/config-local.fish
@@ -0,0 +1,4 @@
+ # The next line updates PATH for the Google Cloud SDK.
+if [ -f '/Users/mauricio.dentz/google-cloud-sdk/path.fish.inc' ]; . '/Users/mauricio.dentz/google-cloud-sdk/path.fish.inc'; end
+direnv hook fish | source
+fish_vi_key_bindings
