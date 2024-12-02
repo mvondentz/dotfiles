2f55a4514242d809cc60d5f0c09c96e96176ea2b
Mauricio Von Dentz <mauriciovondentz@gmail.com>
2024-12-02 16:12:14 -0300

    better illuminate

--- macos/.config/nvim/lua/plugins/illumitate.lua
+++ macos/.config/nvim/lua/plugins/illumitate.lua
@@ -1,3 +1,10 @@
 return {
     'RRethy/vim-illuminate',
+    config = function()
+        vim.cmd([[
+            highlight IlluminatedWordText guibg=#333333 gui=bold
+            highlight IlluminatedWordRead guibg=#333333 gui=bold
+            highlight IlluminatedWordWrite guibg=#444444 gui=bold
+        ]])
+    end,
 }
